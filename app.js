cc8c7d54e35c528f22ddf41bf1f464945ae5381b
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-08-05 15:34:34 -0400

    Add function to (hopefully) flip through pictures

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 const buttons = document.querySelectorAll(".photo-content > buttons");
 const content = document.querySelectorAll(".container > *")
+
+buttons.forEach(button=>{
+    button.addEventListener("click", function(e){
+        const id = this.dataset.target
+        const target = document.querySelector(id);
+        content.forEach(ele=>{
+            if(ele === target) ele.classList.add("active");
+            else ele.classList.remove("active");
+        });
+    });
+});
