11204e4618cfce19249a5f1b2c2be675252b6a49
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-08-05 15:38:22 -0400

    Add missing :

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const buttons = document.querySelectorAll(".photo-content > buttons");
-const content = document.querySelectorAll(".container > *")
+const buttons = document.querySelectorAll(".photo-content > button");
+const content = document.querySelectorAll(".container > *");
 
-buttons.forEach(button=>{
-    button.addEventListener("click", function(e){
-        const id = this.dataset.target
+buttons.forEach(button => {
+    button.addEventListener("click", function(e) {
+        const id = this.dataset.target;
         const target = document.querySelector(id);
-        content.forEach(ele=>{
+        content.forEach(ele => {
             if(ele === target) ele.classList.add("active");
             else ele.classList.remove("active");
         });
