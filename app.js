e67b57a5d041bc59412df200d1762d307c0e95ad
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-08-07 19:40:28 -0400

    Add frame for prev() and next()

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ const slider_img = document.querySelector('.slider-image');
 const images = ['garden.jpg', 'strawberry.jpg', 'kitties.jpg'];
 
 const i = 0;
+
+function prev(){
+
+}
+
+function next(){
+    
+}
