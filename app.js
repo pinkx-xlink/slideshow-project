3ff0155c92018058097fd0175530cd3e8b308481
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-08-07 18:42:54 -0400

    Add another image to script

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 let images = [
     'https://images.unsplash.com/photo-1506792006437-256b665541e2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',
-    'https://images.unsplash.com/photo-1601280929102-6935eec5e05a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHBpeGVsJTIwYXJ0fGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60'
+    'https://images.unsplash.com/photo-1601280929102-6935eec5e05a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHBpeGVsJTIwYXJ0fGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60',
+    'https://images.unsplash.com/photo-1492037766660-2a56f9eb3fcb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzR8fHBpeGVsJTIwYXJ0fGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60'
 ];
-let myPics = document.querySelector('.imageTag');
+let myPics = document.querySelector('.myPics');
 let i = 0;
 
 function next() {
