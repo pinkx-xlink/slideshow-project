339ea92f861473ef72a6c542db50126fe038688e
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-08-07 19:39:42 -0400

    Add const's for images

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-const slider_img = document.querySelector('.slider-image')
+const slider_img = document.querySelector('.slider-image');
+const images = ['garden.jpg', 'strawberry.jpg', 'kitties.jpg'];
+
+const i = 0;
