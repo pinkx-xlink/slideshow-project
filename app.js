e13bd96e378b109a7114cd72170b0752c25f1a9f
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-08-07 18:32:53 -0400

    Add one cover image

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-// let images = [
-//     'paint-brushes.jpg'
-// ];
+ let images = [
+    'https://images.unsplash.com/photo-1506792006437-256b665541e2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80'
+];
 let imageTag = document.querySelector('.imageTag');
 let i = 0;
 
