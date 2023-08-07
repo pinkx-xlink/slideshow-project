fcb4bec678e76d954b0d90770cd4180bdc8c15db
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-08-07 19:44:48 -0400

    Write prev() function

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const images = ['garden.jpg', 'strawberry.jpg', 'kitties.jpg'];
 const i = 0;
 
 function prev(){
-
+    if(i <= 0 ) i = images.length;
+    i--;
+    return setImg();
 }
 
 function next(){
-    
+
 }
