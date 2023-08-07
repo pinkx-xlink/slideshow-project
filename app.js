2466f020b6312d286eaf647b99c8c03ac38a253d
pinkx-xlink <132025012+pinkx-xlink@users.noreply.github.com>
2023-08-07 18:45:01 -0400

    Change images to "pics"

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let images = [
+let pics = [
     'https://images.unsplash.com/photo-1506792006437-256b665541e2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',
     'https://images.unsplash.com/photo-1601280929102-6935eec5e05a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHBpeGVsJTIwYXJ0fGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60',
     'https://images.unsplash.com/photo-1492037766660-2a56f9eb3fcb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzR8fHBpeGVsJTIwYXJ0fGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60'
@@ -7,16 +7,16 @@ let myPics = document.querySelector('.myPics');
 let i = 0;
 
 function next() {
-    if (i >= images.length - 1) {
+    if (i >= pics.length - 1) {
         return false;
     }
     i++;
-    myPics.setAttribute('src', images[i]);
+    myPics.setAttribute('src', pics[i]);
 }
 function previous() {
     if (i <= 0) {
         return false;
     }
     i--;
-    myPics.setAttribute('src', images[i]);
+    myPics.setAttribute('src', pics[i]);
 }
